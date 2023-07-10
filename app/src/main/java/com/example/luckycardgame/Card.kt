9b5a381048f679e5259ce8cffae5dde6142bc115
jyh <jo990714@naver.com>
2023-07-10 17:54:20 +0900

    Card 객체 수정 및 출력 형식 수정

--- app/src/main/java/com/example/luckycardgame/Card.kt
+++ app/src/main/java/com/example/luckycardgame/Card.kt
@@ -2,8 +2,8 @@ package com.example.luckycardgame
 
 // 제한적인 subclass 조건
 // String("동물이름")을 통한 동물 구별 대신 instance를 활용
-sealed class Card {
-    data class Dog(val num : Int) : Card()
-    data class Cat(val num : Int) : Card()
-    data class Cow(val num : Int) : Card()
+sealed class Card (open val cardNum : Int) {
+    data class Dog(override val cardNum : Int, val unicode: String = "\uD83D\uDC36") : Card(cardNum)
+    data class Cat(override val cardNum : Int, val unicode: String = "\uD83D\uDC31") : Card(cardNum)
+    data class Cow(override val cardNum : Int, val unicode: String = "\uD83D\uDC2E") : Card(cardNum)
 }
--- app/src/main/java/com/example/luckycardgame/LuckyCard.kt
+++ app/src/main/java/com/example/luckycardgame/LuckyCard.kt
@@ -1,25 +1,15 @@
 package com.example.luckycardgame
 
-import java.util.Collections
-
-// 유니코드 활용의 편리함을 위해서 enum class 사용
-enum class Animal(val unicode : Int){
-    DOG(0x1F436),
-    CAT(0x1F431),
-    COW(0x1F42E)
-}
-
-fun main(){
-
+fun main() {
     //  0 : Dog, 1 : Cat, 2 :Cow
     val animalRange = (0..2)
-    var rand : Int
-    val numList = MutableList<Int>(12) { i -> i + 1}
-    numList.shuffle() // 1 - 12 사이 중복 없는 랜덤 숫자를 위한 shuffle
+    var rand: Int
+    val numList = MutableList<Int>(12) { i -> i + 1 }
+    numList.shuffle()
 
     numList.forEachIndexed { idx, num ->
         rand = animalRange.random()
-        when(rand){
+        when (rand) {
             0 -> {
                 val dog = Card.Dog(num)
                 printItem(dog)
@@ -27,24 +17,23 @@ fun main(){
             1 -> {
                 val cat = Card.Cat(num)
                 printItem(cat)
-
             }
+
             2 -> {
                 val cow = Card.Cow(num)
                 printItem(cow)
             }
         }
-
-        if(idx != 11){
+        if (idx != 11) {
             print(", ")
         }
     }
 }
 
-private fun printItem (item : Card){
-    when(item){
-        is Card.Dog -> print(String.format("${String(Character.toChars(Animal.DOG.unicode))}%02d",item.num))
-        is Card.Cat -> print(String.format("${String(Character.toChars(Animal.CAT.unicode))}%02d",item.num))
-        is Card.Cow -> print(String.format("${String(Character.toChars(Animal.COW.unicode))}%02d",item.num))
+private fun printItem(item: Card) {
+    when (item) {
+        is Card.Dog -> print(String.format("%s%02d", item.unicode, item.cardNum))
+        is Card.Cat -> print(String.format("%s%02d", item.unicode, item.cardNum))
+        is Card.Cow -> print(String.format("%s%02d", item.unicode, item.cardNum))
     }
 }

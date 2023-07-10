ad3cc77e911f588e1f550057bd9b77a783a9bbce
jyh <jo990714@naver.com>
2023-07-10 17:53:11 +0900

    빈 줄 제거

--- app/src/main/java/com/example/luckycardgame/MainActivity.kt
+++ app/src/main/java/com/example/luckycardgame/MainActivity.kt
@@ -2,18 +2,10 @@ package com.example.luckycardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import java.util.Collections.shuffle
-import java.util.Random
-
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
-
 }

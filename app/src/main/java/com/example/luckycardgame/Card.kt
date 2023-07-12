9804a46965316b0c23f3c6aabfabc09f5f87eaf0
jyh <jo990714@naver.com>
2023-07-12 16:51:33 +0900

    viewbinding을 활용하도록 수정 & 카드 객체 유니코드 형식 수정

--- app/src/main/java/com/example/luckycardgame/Card.kt
+++ app/src/main/java/com/example/luckycardgame/Card.kt
@@ -1,7 +1,7 @@
 package com.example.luckycardgame
 
 sealed class Card (open val cardNum : Int, open var flipped : Boolean){
-    data class Dog(override val cardNum: Int, override var flipped: Boolean = true, val unicode: Int = 0x1F436) : Card(cardNum, flipped)
-    data class Cat(override val cardNum: Int, override var flipped: Boolean = true, val unicode: Int = 0x1F431) : Card(cardNum, flipped)
-    data class Cow(override val cardNum: Int, override var flipped: Boolean = true, val unicode: Int = 0x1F42E) : Card(cardNum, flipped)
+    data class Dog(override val cardNum: Int, override var flipped: Boolean = true, val unicode: String = "\uD83D\uDC36") : Card(cardNum, flipped)
+    data class Cat(override val cardNum: Int, override var flipped: Boolean = true, val unicode: String = "\uD83D\uDC31") : Card(cardNum, flipped)
+    data class Cow(override val cardNum: Int, override var flipped: Boolean = true, val unicode: String = "\uD83D\uDC2E") : Card(cardNum, flipped)
 }
--- app/src/main/java/com/example/luckycardgame/CardListAdapter.kt
+++ app/src/main/java/com/example/luckycardgame/CardListAdapter.kt
@@ -3,23 +3,23 @@ package com.example.luckycardgame
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.luckycardgame.databinding.CardItemBinding
 
 class CardListAdapter(private val cardList: MutableList<Card>) :
     RecyclerView.Adapter<CardListAdapter.CardListViewHolder>() {
 
-    inner class CardListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val topNum = itemView.findViewById<TextView>(R.id.top_num)
-        val bottomNum = itemView.findViewById<TextView>(R.id.bottom_num)
-        val animalImg = itemView.findViewById<TextView>(R.id.animal_icon)
-        val backsideImg = itemView.findViewById<ImageView>(R.id.backside_img)
+    inner class CardListViewHolder(binding: CardItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val topNum = binding.topNum
+        val bottomNum = binding.bottomNum
+        val animalImg = binding.animalIcon
+        val backsideImg = binding.backsideImg
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardListViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
-        return CardListViewHolder(view)
+        val binding = CardItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CardListViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -40,23 +40,21 @@ class CardListAdapter(private val cardList: MutableList<Card>) :
                 is Card.Dog -> {
                     holder.topNum.text = (cardItem as Card.Dog).cardNum.toString()
                     holder.bottomNum.text = cardItem.cardNum.toString()
-                    holder.animalImg.text = String(Character.toChars(cardItem.unicode))
+                    holder.animalImg.text = cardItem.unicode
                 }
 
                 is Card.Cat -> {
                     holder.topNum.text = (cardItem as Card.Cat).cardNum.toString()
                     holder.bottomNum.text = cardItem.cardNum.toString()
-                    holder.animalImg.text = String(Character.toChars(cardItem.unicode))
+                    holder.animalImg.text = cardItem.unicode
                 }
 
                 is Card.Cow -> {
                     holder.topNum.text = (cardItem as Card.Cow).cardNum.toString()
                     holder.bottomNum.text = cardItem.cardNum.toString()
-                    holder.animalImg.text = String(Character.toChars(cardItem.unicode))
+                    holder.animalImg.text = cardItem.unicode
                 }
-
             }
         }
-
     }
 }

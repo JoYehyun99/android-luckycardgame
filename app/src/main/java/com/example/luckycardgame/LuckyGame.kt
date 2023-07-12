a6414b93016f2ef3d4be4dff48674ea0a95d50fb
jyh <jo990714@naver.com>
2023-07-12 11:57:27 +0900

    Game 종료 조건 확인 메서드 & 테스트 구현

--- app/src/main/java/com/example/luckycardgame/LuckyGame.kt
+++ app/src/main/java/com/example/luckycardgame/LuckyGame.kt
@@ -1,6 +1,12 @@
 package com.example.luckycardgame
 
+import kotlin.math.abs
+
 class LuckyGame {
+
+    companion object{
+        const val GOAL_NUM = 7
+    }
 
     var participantsCnt = 3
     var participantCardCnt = 8
@@ -8,6 +14,7 @@ class LuckyGame {
     val totalCardList: MutableList<Card> = mutableListOf()
     val totalCardListForThree: MutableList<Card> = mutableListOf()
     var bottomCardList: MutableList<Card> = mutableListOf()
+    val winnerList : MutableMap<Int, Participant> = mutableMapOf()
 
     init {
         for (i in 1..12) {
@@ -97,16 +104,17 @@ class LuckyGame {
         return bottomCardList
     }
 
-    fun findWhoHasSameThreeCard(): Map<Participant, List<Int>> {
-
-        val result: MutableMap<Participant, List<Int>> = mutableMapOf()
+    fun findWhoHasSameThreeCard(): Map<Int, Participant> {
+        val result: MutableMap<Int,Participant> = mutableMapOf()
 
         participantsList.forEach { participant ->
             val cardCnt = participant.ownCardList.groupingBy { it.cardNum }.eachCount()
-            val sameThreeCardNums = cardCnt.filter { it.value == 3 }.keys.toList()
+            val sameThreeCards = cardCnt.filter { it.value == 3 }.keys.toList()
 
-            if (sameThreeCardNums.isNotEmpty()) {
-                result[participant] = sameThreeCardNums
+            if (sameThreeCards.isNotEmpty()) {
+                sameThreeCards.forEach { cardNum ->
+                    result[cardNum] = participant
+                }
             }
         }
         return result
@@ -117,7 +125,6 @@ class LuckyGame {
         secondUserId: Int,
         bottomCardNum: Int = bottomCardList.random().cardNum
     ): Int {
-
         val firstUser = participantsList[firstUserId]
         val secondUser = participantsList[secondUserId]
 
@@ -137,7 +144,6 @@ class LuckyGame {
         if (firstUserMaxCardNum == secondUserMaxCardNum) {
             if (bottomCardNum == firstUserMaxCardNum) return bottomCardNum
         }
-
         return -1
     }
 
@@ -156,4 +162,71 @@ class LuckyGame {
             false
         }
     }
+
+    fun checkAllCardsFlipped(): Boolean{
+        return participantsList.all { participant ->
+            participant.ownCardList.all { card -> !card.flipped }
+        }
+    }
+
+    fun combination(
+        answer: MutableList<List<Int>>,
+        elements: List<Int>,
+        count: Int,
+        start: Int,
+        used : Array<Boolean>
+    ){
+        if(count==0){
+            answer.addAll(listOf(elements.filterIndexed{idx, t -> used[idx]}))
+        } else{
+            for(i in start until elements.size){
+                used[i] = true
+                combination(answer,elements,count-1,i+1,used)
+                used[i] = false
+            }
+        }
+    }
+
+    fun checkCombinationForSumOrDiffSeven(combination : List<List<Int>>): List<Int>?{
+        for (pair in combination){
+            val sum = pair.sum()
+            val diff = abs(pair[0] - pair[1])
+            if(sum == 7 || diff == 7){
+                return pair
+            }
+        }
+        return null
+    }
+
+    fun isGameEnd(): Boolean{
+
+        val sameCards = findWhoHasSameThreeCard()
+
+        if(sameCards.isNotEmpty()){
+            val numList = sameCards.keys.toList()
+
+            if(numList.contains(GOAL_NUM)) { // 7을 가지고 있는 이용자가 있는 경우
+                winnerList[GOAL_NUM] = sameCards.getValue(GOAL_NUM)
+                return true
+            } else{ // 7외의 숫자들만 있는 경우 -> 합/차 조합 테스트 해보기
+                for (i in 2..numList.size){
+                    val combinationList = mutableListOf<List<Int>>()
+                    combination(combinationList,numList, i, 0, Array(numList.size) { false })
+
+                    val result = checkCombinationForSumOrDiffSeven(combinationList)
+                    if(result != null){
+                        // 승자 추가
+                        result.forEach{ num ->
+                            winnerList[num] = sameCards.getValue(num)
+                        }
+                        return true
+                    }
+                }
+            }
+        }
+        return checkAllCardsFlipped()
+    }
+
+
+
 }
--- app/src/test/java/com/example/luckycardgame/LuckyCardTest.kt
+++ app/src/test/java/com/example/luckycardgame/LuckyCardTest.kt
@@ -7,11 +7,13 @@ import org.junit.Before
 class LuckyCardTest {
 
     private lateinit var luckyGame: LuckyGame
+
     @Before
     fun setupLuckyGame() {
         luckyGame = LuckyGame()
         luckyGame.setParticipantsNumbers(3) // 게임 인원을 3명으로 임의로 설정
     }
+
     @Test
     fun initialSetting_countAnimalCards_eachHas12Cards() {
         val groupByAnimal = luckyGame.totalCardList.groupBy { card ->
@@ -26,11 +28,13 @@ class LuckyCardTest {
         val answer = arrayOf(12, 12, 12)
         assertArrayEquals(answer, groupByAnimal.values.toTypedArray())
     }
+
     @Test
     fun initialSetting_countAnimalCardsFor3People_NoNumber12() {
         val cardSet = luckyGame.totalCardListForThree
         assertTrue(cardSet.all { it.cardNum < 12 })
     }
+
     @Test
     fun shareCard_toParticipants_properly() {
         luckyGame.setParticipantsNumbers(3)
@@ -42,6 +46,7 @@ class LuckyCardTest {
         luckyGame.setParticipantsNumbers(5)
         assertEquals(6, luckyGame.participantsList[0].ownCardList.size)
     }
+
     @Test
     fun shareCard_toBottom_properly() {
         luckyGame.setParticipantsNumbers(3)
@@ -53,6 +58,7 @@ class LuckyCardTest {
         luckyGame.setParticipantsNumbers(5)
         assertEquals(6, luckyGame.bottomCardList.size)
     }
+
     @Test
     fun sortCard_byParticipant_inAscendingOrder() {
         val userId = 1
@@ -63,6 +69,7 @@ class LuckyCardTest {
         }.all { it }
         assertTrue(testCardList)
     }
+
     @Test
     fun sortCard_bottom_inAscendingOrder() {
         luckyGame.sortBottomCardByNum()
@@ -71,14 +78,16 @@ class LuckyCardTest {
         }.all { it }
         assertTrue(testCardList)
     }
+
     @Test
     fun findParticipant_WhoHasSameThreeCard_whoAndWhatNumbers() {
         val result = luckyGame.findWhoHasSameThreeCard()
-        result.forEach { (participant, cardNums) ->
-            val tmp = participant.ownCardList.filter { card -> cardNums.contains(card.cardNum) }
+        result.forEach { (cardNum, participant) ->
+            val tmp = participant.ownCardList.filter { card -> card.cardNum == cardNum }
             assertTrue(tmp.size % 3 == 0)
         }
     }
+
     @Test
     fun compareThreeCards_twoParticipantsAndBottom_isTrue() {
 
@@ -98,7 +107,7 @@ class LuckyCardTest {
     }
 
     @Test
-    fun flipCard_BothEndsCard_returnTrue(){
+    fun flipCard_BothEndsCard_returnTrue() {
         val user = 1
         val left = 0
         val right = luckyGame.participantCardCnt - 1
@@ -108,19 +117,61 @@ class LuckyCardTest {
     }
 
     @Test
-    fun flipCard_middleCards_returnFalse(){
+    fun flipCard_middleCards_returnFalse() {
         val user = 1
         val position = 2
 
-        assertFalse(luckyGame.flipCard(user,position))
+        assertFalse(luckyGame.flipCard(user, position))
     }
 
     @Test
-    fun flipCard_flipCardsConsecutively_returnTrue(){
+    fun flipCard_flipCardsConsecutively_returnTrue() {
         val user = 1
-        for (i in 0..1){
-            assertTrue(luckyGame.flipCard(user,i))
+        for (i in 0..1) {
+            assertTrue(luckyGame.flipCard(user, i))
         }
     }
+
+    @Test
+    fun checkAllCardsFlipped_NotAllFlipped_returnFalse() {
+        assertFalse(luckyGame.checkAllCardsFlipped())
+    }
+
+    @Test
+    fun checkAllCardsFlipped_AllFlipped_returnTrue() {
+        luckyGame.participantsList.forEach { participant ->
+            participant.ownCardList.forEach { card ->
+                card.flipped = false
+            }
+        }
+        assertTrue(luckyGame.checkAllCardsFlipped())
+    }
+
+    @Test
+    fun combination_returnCorrectResult_withNoDuplicate() {
+        val test = listOf(1, 2, 6)
+        val cnt = 2
+        val actual = mutableListOf<List<Int>>()
+        val expected = listOf(listOf(1,2), listOf(1,6), listOf(2,6))
+        luckyGame.combination(actual, test, cnt, 0, Array(test.size) { false })
+        print(actual.toString())
+
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun combination_checkSumOrDiff_returnTrueIfSeven(){
+        val test = listOf(listOf(1,2,4), listOf(1,2,6), listOf(2,6,8))
+        val expected = listOf(1,2,4)
+
+        assertEquals(expected,luckyGame.checkCombinationForSumOrDiffSeven(test))
+    }
+
+    @Test
+    fun isGameEnd_checkGameStatus(){
+        assertFalse(luckyGame.isGameEnd())
+    }
+
+
 
 }
